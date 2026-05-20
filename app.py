4a7960b9e7d591641a6685065742e896dd14d822
Pratham1115 <prathamprasad2005@gmail.com>
2026-05-20 23:35:17 +0530

    Initial project setup

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Customer Segmentation",
+    layout="wide"
+)
+
+st.title("AI-Powered Customer Segmentation System")
+
+st.write("Welcome to the AI Dashboard")
